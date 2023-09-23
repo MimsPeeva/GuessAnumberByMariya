0552b630325d8f3410158caa962b62202af08dc1
MimsPeeva <mpeeva04@abv.bg>
2023-09-23 22:04:07 +0300

    Add program

---
+++ GuessAnumber/GuessAnumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessAnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int computerNumber = number.Next(1,101);
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!"); break;
+                    }
+                    else if (playerNumber > computerNumber)
+                    { Console.WriteLine("Too High"); }
+                    else if (playerNumber < computerNumber)
+                    { Console.WriteLine("Too Low"); }
+                }
+                else { Console.WriteLine("Invalid input."); }
+               
+            }
+        }
+    }
+}
